2627f7d9e5f0bfacbf7662869095db59248e13bc
zachj4 <134519601+zachj4@users.noreply.github.com>
2024-10-17 13:56:46 -0400

    Update lxc-deploy.sh

--- lxc-deploy.sh
+++ lxc-deploy.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
-
 # Update LXC Container and minimize SSD wear.
-
 # Perform full upgrade
-
 apt update
 apt -y full-upgrade
-
 # Only swap to prevent SSD wear
-
 echo "# only swap to prevent OOM" >> /etc/sysctl.d/sysctl.conf
 echo "vm.swappiness = 0" >> /etc/sysctl.d/sysctl.conf
-
 # Install unattended-upgrades to automatically keep LXC up to date.
-
 apt-get update
 apt-get install unattended-upgrades apt-listchanges
 cp /usr/share/systemd/tmp.mount /etc/systemd/system/
 systemctl enable tmp.mount
-
 # Configure LXC time zone
-
-timedatectl set-timezone America/Kentucky/Louisville
+# timedatectl set-timezone America/Kentucky/Louisville
