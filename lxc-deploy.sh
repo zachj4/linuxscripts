d0d73e3f34bc8eb17890c1764175ac77d14b830a
zachj4 <134519601+zachj4@users.noreply.github.com>
2024-10-24 10:45:55 -0400

    Update lxc-deploy.sh

--- lxc-deploy.sh
+++ lxc-deploy.sh
@@ -2,24 +2,43 @@
 
 # Update LXC Container and minimize SSD wear.
 
+echo "===LXC Deploy Script - zjones==="
+
 # Perform full upgrade
 
+echo "Performing full upgrade..."
 apt update
 apt -y full-upgrade
 
-# Only swap to prevent SSD wear
+# Configure swap and tmp to prevent SSD wear
 
+echo "Configuring swap and tmp to prevent SSD wear..."
 echo "# only swap to prevent OOM" >> /etc/sysctl.d/sysctl.conf
 echo "vm.swappiness = 0" >> /etc/sysctl.d/sysctl.conf
-
-# Install unattended-upgrades to automatically keep LXC up to date.
-
-apt-get update
-apt-get install unattended-upgrades apt-listchanges
-
 cp /usr/share/systemd/tmp.mount /etc/systemd/system/
 systemctl enable tmp.mount
 
+# Install unattended-upgrades to automatically keep LXC up to date
+
+echo "Installing unattended-upgrades..."
+apt-get update
+apt-get install unattended-upgrades apt-listchanges
+
+# Add package-upgrade.sh to crontab
+
+echo "Running script to add package-upgrade.sh script to crontab..."
+wget -O /tmp/auto-upgrades.sh https://raw.githubusercontent.com/zachj4/linuxscripts/refs/heads/main/auto-upgrades.sh
+chmod a+x /tmp/auto-upgrades.sh
+./tmp/auto-upgrades.sh
+
+echo "Auto-upgrades script completed..."
+
 # Configure LXC time zone
 
+echo "Configuring time-zone to Eastern Time..."
+
 timedatectl set-timezone America/Kentucky/Louisville
+
+# Completed
+
+echo "Finished!"
